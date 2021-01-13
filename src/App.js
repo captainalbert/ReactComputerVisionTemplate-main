fa6616c463c8da686c7acd6c803b1453f0fbe4ea
Lester016 <deguzmanlester.016@gmail.com>
2021-01-13 22:26:01 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
   const [score, setScore] = useState(0);
   const [onObjectChange, setOnObjectChange] = usestate([]);
 
-
+  console.log("TESTING", onObjectChange);
   const classes = useStyles();
 
   // Main function
